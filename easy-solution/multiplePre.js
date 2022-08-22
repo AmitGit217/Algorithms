86e4949b013cc4278a976c328a5ca9473698f310
Amit Bar-Gil <amit217@yandex.com>
2022-08-22 16:29:48 +0300

    Multi presistence

---
+++ easy-solution/multiplePre.js
@@ -0,0 +1,13 @@
+function multiplicativePersistence(num) {
+    const numbersArr = num.toString().split("");
+    function recurse(arr, counter) {
+        counter = counter || 0;
+        if (arr.length < 2) {
+            return counter;
+        } else {
+            arr = arr.reduce((a, b) => parseInt(a) * parseInt(b), 1);
+            return recurse(arr.toString().split(""), counter + 1);
+        }
+    }
+    return recurse(numbersArr);
+}
