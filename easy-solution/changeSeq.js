b721d4f3be0178090f263967cb312522feec30e5
Amit Bar-Gil <amit217@yandex.com>
2022-08-23 08:45:15 +0300

    Change sequence

---
+++ easy-solution/changeSeq.js
@@ -0,0 +1,18 @@
+function changeSequence(arr) {
+    let increasing = arr[1] > arr[0] ? true : false;
+    let stateChange = false;
+    let target = 0;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (increasing && arr[i + 1] > arr[i]) {
+            continue;
+        }
+        if (!increasing && arr[i + 1] < arr[i]) {
+            continue;
+        } else {
+            stateChange = true;
+            return (target = i);
+        }
+    }
+    return stateChange ? target : -1;
+}
