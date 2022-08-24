a8cde02504368ecb54323e0c0a4160bd6ec8d854
Amit Bar-Gil <amit217@yandex.com>
2022-08-24 22:20:13 +0300

    Binary reversal

---
+++ easy-solution/binaryReverse.js
@@ -0,0 +1,23 @@
+function binaryReversal(str) {
+    let number = parseInt(str);
+    let unicode = [];
+    function toBinaryRecursion(n) {
+        if (n < 1) {
+            return unicode;
+        }
+        n % 2 == 0 ? unicode.unshift(0) : unicode.unshift(1);
+        n = n / 2;
+        return toBinaryRecursion(Math.floor(n));
+    }
+    toBinaryRecursion(number);
+    while (unicode.length % 8 != 0) {
+        unicode.unshift(0);
+    }
+    let result = 0;
+    for (let i = unicode.length - 1; i >= 0; i--) {
+        if (unicode[i] == 1) {
+            result += Math.pow(2, i);
+        }
+    }
+    return result;
+}
