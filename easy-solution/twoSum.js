da768b9c5be235dd466553a3e121393f87068bae
Amit Bar-Gil <amit217@yandex.com>
2022-08-26 16:10:30 +0300

    Two sum

---
+++ easy-solution/twoSum.js
@@ -0,0 +1,16 @@
+function TwoSum(arr) {
+    const map = new Map();
+    const target = arr.shift();
+    const result = [];
+    arr.forEach((number, index) => map.set(number, index));
+
+    arr.forEach((number, index) => {
+        const gap = target - number;
+        if (map.has(gap) && map.get(gap) != index) {
+            result.push([number, gap]);
+            map.delete(number);
+        }
+    });
+
+    return result.length ? result.join(" ") : -1;
+}
