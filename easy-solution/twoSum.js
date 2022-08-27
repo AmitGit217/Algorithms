5f1c2367c0bf84dcfc12d421f6c8c031c9df1c7a
Amit Bar-Gil <amit217@yandex.com>
2022-08-27 12:36:58 +0300

    Product digits

--- easy-solution/twoSum.js
+++ easy-solution/twoSum.js
@@ -3,7 +3,6 @@ function TwoSum(arr) {
     const target = arr.shift();
     const result = [];
     arr.forEach((number, index) => map.set(number, index));
-
     arr.forEach((number, index) => {
         const gap = target - number;
         if (map.has(gap) && map.get(gap) != index) {
@@ -11,6 +10,5 @@ function TwoSum(arr) {
             map.delete(number);
         }
     });
-
     return result.length ? result.join(" ") : -1;
 }
