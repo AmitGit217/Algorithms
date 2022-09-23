9f208ee99fa66c228887371a3f7b00ae9c112403
Amit Bar-Gil <amit217@yandex.com>
2022-09-23 23:30:26 +0300

    Tree constructor with loop getter

--- medium/solution/treeConstructor.ts
+++ medium/solution/treeConstructor.ts
@@ -1,4 +1,5 @@
-function TreeConstructor(strArr: string[]) {
+function TreeConstructorTwo(strArr: string[]) {
+    if (!strArr.length) return true;
     const numArr = strArr
         .map((item) => item.match(/\d+/g))
         .map((item) => [parseInt(item![0]), parseInt(item![1])]);
@@ -13,7 +14,7 @@ function TreeConstructor(strArr: string[]) {
         if (parents[parent]) {
             parents[parent].push(child);
         } else {
-            parents[parent] = [couple[0]];
+            parents[parent] = [child];
         }
         if (children[child]) {
             children[child].push(parent);
@@ -21,6 +22,22 @@ function TreeConstructor(strArr: string[]) {
             children[child] = [parent];
         }
     });
+    const loopGetter = new Set<number>();
+    let loop = false;
+    function recurse(item: any, i: number) {
+        if (i >= numArr.length) {
+            return;
+        } else if (loopGetter.has(item)) {
+            loop = true;
+            return;
+        } else {
+            if (parents[item] !== undefined) {
+                loopGetter.add(item);
+                return recurse(parents[item].toString(), i + 1);
+            }
+        }
+    }
+    recurse(parents[numArr[0][1]].toString(), 0);
     numArr.forEach((item) =>
         !children.hasOwnProperty(item[1]) ? root.add(item[1]) : null
     );
@@ -32,7 +49,7 @@ function TreeConstructor(strArr: string[]) {
     );
     const findFalseRoots = root.size !== 1;
 
-    return findFalseParents || findFalseChildren || findFalseRoots
+    return findFalseParents || findFalseChildren || findFalseRoots || loop
         ? false
         : true;
 }
